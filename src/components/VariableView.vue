abf57d90ecf5a577ede817841a574550631436aa
George Kinsman <george@georgekinsman.com>
2023-08-01 18:31:58 +0100

    Add copy button

--- src/components/VariableView.vue
+++ src/components/VariableView.vue
@@ -1,12 +1,28 @@
 <template>
   <q-expansion-item dense class="q-my-xs" :label="props.variable.key">
     <q-item-section class="q-pl-md text-weight-medium">
-      {{ variable.value }}
+      <div>
+        <q-btn
+          class="q-pa-xs"
+          clickable
+          square
+          size="xs"
+          flat
+          @click="copy"
+          icon="content_copy"
+        ></q-btn>
+        {{ variable.value }}
+      </div>
     </q-item-section>
   </q-expansion-item>
 </template>
 <script lang="ts" setup>
 import { VariableSchema } from '@gitbeaker/core/dist/types/templates/types'
+import { copyToClipboard } from 'quasar'
+
+function copy() {
+  copyToClipboard(props.variable.value)
+}
 
 const props = defineProps<{
   variable: VariableSchema
